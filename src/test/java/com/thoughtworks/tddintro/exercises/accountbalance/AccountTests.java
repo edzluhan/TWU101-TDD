aa66bff6061ac64d07be2ff86f75545c1c8649f3
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2019-03-29 17:14:02 -0300

    refactor tests to use class constructor

--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -9,27 +9,24 @@ public class AccountTests {
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
 
-        Account account = new Account();
-        assertThat(account.getBalance(), is(0.0));
+        Account account = new Account(100.00);
         account.deposit(50.0);
-        assertThat(account.getBalance(), is(50.0));
+        assertThat(account.getBalance(), is(150.00));
     }
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 
-        Account account = new Account();
-        account.deposit(600.0);
-        account.withdraw(500.0);
-        assertThat(account.getBalance(), is(100.0));
+        Account account = new Account(100.00);
+        account.withdraw(50.0);
+        assertThat(account.getBalance(), is(50.00));
     }
 
     @Test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 
-        Account account = new Account();
-        account.deposit(500.0);
-        account.withdraw(600.0);
-        assertThat(account.getBalance(), is(500.0));
+        Account account = new Account(50.00);
+        account.withdraw(100.0);
+        assertThat(account.getBalance(), is(50.0));
     }
 }
