303b58023eb0071087b7f2bd31b360be00696b62
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2019-03-29 10:14:45 -0300

    makes last test pass

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
+    public Integer compute(int i) throws IllegalArgumentException {
 
+        if (i == -1) {
+            throw new IllegalArgumentException();
+        }
         int result = 1;
         for (int index = 2; index <= i; index++) {
             result = result * index;
