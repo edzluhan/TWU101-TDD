5aefc883681be0c7c2beb413ec342a37db0dc69a
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2019-03-29 10:40:53 -0300

    increase balance when deposit on account

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+
+    private double balance = 0.0;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,11 +3,17 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+
         Account account = new Account();
+        assertThat(account.getBalance(), is(0.0));
+        account.deposit(50.0);
+        assertThat(account.getBalance(), is(50.0));
     }
 
     @Test
