42a59c54645de6b657d2985bb2f1aea6ef6217bd
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2019-03-29 17:13:16 -0300

    refactor class: use constructor to set initial account balance

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -2,7 +2,15 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
 
-    private double balance = 0.0;
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public Account() {
+        this.balance = 0.0;
+    }
 
     public double getBalance() {
         return balance;
